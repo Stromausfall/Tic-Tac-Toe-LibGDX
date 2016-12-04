b3da0d401c2b87ff43d3d4cc64d7c892e86a3539
Matthias Auer <mail@matthias-auer.net>
2016-12-04 08:32:47 +0100

    inital commit from generated libgdx project

---
+++ desktop/src/net/matthiasauer/libgdx/tictactoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.matthiasauer.libgdx.tictactoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.matthiasauer.libgdx.tictactoe.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
